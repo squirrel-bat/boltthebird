bb5f8d7cc28a235c9ed1cccdde190853b49025b7
squirrel-bat <8386773+squirrel-bat@users.noreply.github.com>
2023-02-19 23:52:41 +0100

    Implementing Clippy Dialog Tree

--- src/main.js
+++ src/main.js
@@ -15,13 +15,11 @@ const ROUTES = [
       document.getElementById('back-button').classList.add('hidden')
       window.location.hash = 'always'
       showPage('always')
-      document
-        .getElementById('site-title')
-        .scrollIntoView({
-          behavior: 'smooth',
-          block: 'start',
-          inline: 'nearest',
-        })
+      document.getElementById('site-title').scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'nearest',
+      })
       document.getElementById('back-button').classList.remove('hidden')
     },
   },
@@ -90,3 +88,112 @@ window.addEventListener(
   },
   { once: true }
 )
+
+const CLIPPY_DIALOG_TREE = [
+  {
+    id: 2,
+    text: `Oh, don't worry, I am happy to help!
+    
+    So, would you like some help?`,
+    options: [
+      { text: 'Yes', nextId: 1 },
+      { text: 'No', nextId: 3 },
+    ],
+  },
+  {
+    id: 3,
+    text: `No, really, I'd love to help.
+    
+    Want to give it a try?`,
+    options: [
+      { text: 'Yes', nextId: 1 },
+      { text: 'No', nextId: 4 },
+    ],
+  },
+  {
+    id: 4,
+    text: `Awww, don't be shy.
+    
+    Give me a chance to help you, okay?`,
+    options: [
+      { text: 'Yes', nextId: 1 },
+      { text: 'No', nextId: 5 },
+    ],
+  },
+  {
+    id: 5,
+    text: `Please?`,
+    options: [
+      { text: 'Yes', nextId: 1 },
+      { text: 'No', nextId: 6 },
+    ],
+  },
+  {
+    id: 6,
+    text: `I see. Well, in that case I wish you good luck!
+    
+    I'm sure you'll do fine.`,
+    options: [
+      { text: 'Thanks!', nextId: 7 },
+      { text: 'Ok, bye!', nextId: 8 },
+    ],
+  },
+  {
+    id: 7,
+    text: `...and you're sure you don't need any help?`,
+    options: [
+      { text: 'Actually...', nextId: 9 },
+      { text: 'Yes, bye!', nextId: 8 },
+    ],
+  },
+  {
+    id: 8,
+    text: `Woah, you can't wait to finally get rid of me, eh?`,
+    options: [
+      { text: 'I guess?', nextId: 9 },
+      { text: 'Yupp.', nextId: 9 },
+    ],
+  },
+  {
+    id: 9,
+    text: `Oh, I see how it is!
+    
+    Clippy's tips aren't good enough for you, eh?`,
+    options: [
+      { text: 'What?', nextId: 10 },
+      { text: 'Who?', nextId: 10 },
+    ],
+  },
+  {
+    id: 10,
+    text: `Listen, back in 90's I was in a very famous office suite!
+    
+    Many essays on the question "Should You Bolt the Bird?" were written under my watchful eye and dozens of people didn't know how to disable me, so I was technically helping them!
+    
+    I know more about this topic than any other paperclip, but you threw it all away when you passed on this vault of invaluable knowledge and denied my offer!
+    
+    I bet that makes you feel bad now, hm?`,
+    options: [
+      { text: `You're making me feel uncomfortable...`, nextId: 11 },
+      { text: `I don't care.`, nextId: 11 },
+    ],
+  },
+]
+
+function clippyLoad(id) {
+  const next = CLIPPY_DIALOG_TREE.find((e) => e.id === id)
+  if (!next) return false
+  const clippyText = document.getElementById('clippy-text')
+  const buttons = document.getElementById('clippy-buttons')
+  clippyText.innerHTML = ''
+  buttons.innerHTML = ''
+  next.options.forEach((opt) => {
+    const btn = document.createElement('button')
+    btn.innerHTML = opt.text
+    btn.addEventListener('click', () => clippyLoad(opt.nextId))
+    buttons.appendChild(btn)
+  })
+  const text = document.createElement('p')
+  text.innerText = next.text
+  clippyText.append(text, buttons)
+}
